4ddd720e3f7fe189e8ece8c39d50203d523dab4c
muskanbatra-dev <95659002+muskanbatra-dev@users.noreply.github.com>
2025-10-28 01:37:32 +0530

    components initialized

---
+++ src/components/JsonInput.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const JsonInput = () => {
+  return (
+    <div>JsonInput</div>
+  )
+}
+
+export default JsonInput
---
+++ src/components/TreeVisualizer.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const TreeVisualizer = () => {
+  return <div>TreeVisualizer</div>;
+};
+
+export default TreeVisualizer;
